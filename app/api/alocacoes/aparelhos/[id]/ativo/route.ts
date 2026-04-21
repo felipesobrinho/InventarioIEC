b7940a45c1799f071a657c106ddf8050ffa76849
Felipe Nephew <felipe.sobrinho.3@outlook.com>
2026-04-20 21:00:46 -0300

    fix: runtime build error

--- app/api/alocacoes/aparelhos/[id]/ativo/route.ts
+++ app/api/alocacoes/aparelhos/[id]/ativo/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
-
+export const runtime = 'nodejs'
 type Props = { params: Promise<{ id: string }> }
 
 export async function DELETE(_: Request, { params }: Props) {
