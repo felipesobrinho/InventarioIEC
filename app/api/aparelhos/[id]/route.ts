badced994459dab5f7f1590bbbf48af637ec4a88
Felipe Nephew <felipe.sobrinho.3@outlook.com>
2026-04-22 13:38:05 -0300

    fix: set runtime to nodejs in multiple route files

--- app/api/aparelhos/[id]/route.ts
+++ app/api/aparelhos/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
-
+export const runtime = 'nodejs'
 type Props = { params: Promise<{ id: string }> }
 
 export async function GET(request: NextRequest, { params }: Props) {
