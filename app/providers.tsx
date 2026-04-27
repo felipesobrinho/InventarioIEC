a40a61f79bdc70e1f1b0cc4682f5673241cda90a
João Victor Soares Souza <joaovictorsoaressouza@MacBook-Air-de-Joao.local>
2026-04-27 13:08:31 -0300

    feat: add api request toasts

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
+import { ApiRequestToasts } from '@/components/system/api-request-toasts'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         enableSystem={false}
         disableTransitionOnChange
       >
+        <ApiRequestToasts />
         {children}
       </ThemeProvider>
     </SessionProvider>
