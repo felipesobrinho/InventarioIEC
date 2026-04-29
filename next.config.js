abb1f807925c06c944511df11c8ccb48c2ceae4b
Felipe Sobrinho <felipe.sobrinho.3@outlook.com>
2026-04-29 11:07:50 -0300

    refactor: remove unused webpack configuration and canvas alias

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'bcryptjs'],
-
-  turbopack: {},
-
-  webpack: (config) => {
-    if (!config.resolve) {
-      config.resolve = {}
-    }
-
-    if (!config.resolve.alias) {
-      config.resolve.alias = {}
-    }
-
-    config.resolve.alias.canvas = false
-
-    return config
-  },
 }
 
 module.exports = nextConfig
